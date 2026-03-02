9b09207d90f216d9e93675a880e901c184e7df53
Krunal Jumde <krunaljumde24@gmail.com>
2026-03-02 18:41:45 +0530

    added status in response

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.json({ msg: "Hello from express server." });
+  res.json({ msg: "Hello from express server.", status: "SUCCESS" });
 });
 
 app.listen(PORT, () => {
